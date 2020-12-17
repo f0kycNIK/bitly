09293423b2519d29274a67aa7bbeb3e855c858de
Nikolay <70325611+f0kycNIK@users.noreply.github.com>
2020-12-17 12:36:31 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Bitly url shorterer
+```
+C:\Users\pythonProject>python main.py https://docs.python.org/3.6/howto/argparse.html
+Битлинк:  https://bit.ly/2ILIznc
+
+C:\Users\pythonProject>python main.py https://bit.ly/34avnQg
+Общее колличесво кликов:  0
+```
+
+## How to install
+
+Python3 should be already installed. Then use pip (or pip3, if there is a conflict with Python2) to install dependencies:
+```python
+    pip install -r requirements.txt
+```
+## Project Goals
+The code is written for educational purposes on online-course for web-developers dvmn.org.
---
+++ main.py
@@ -0,0 +1,66 @@
+import requests
+import os
+import argparse
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+
+def check_link(user_url, token):
+    if check_bitlink(user_url, token):
+        try:
+            clicks_count = count_cliks(user_url, token)
+            print('Общее колличесво кликов: ', clicks_count)
+        except requests.exceptions.HTTPError:
+            print('Ошибка подсчета кликов битлинка')
+        
+    else:
+        try:
+            bitlink = creating_shorten_link(user_url, token)
+            print('Битлинк: ', bitlink)
+        except requests.exceptions.HTTPError:
+            print('Ошибка создания билтлинка')
+                      
+def creating_shorten_link(user_url, token):
+    url = "https://api-ssl.bitly.com/v4/bitlinks"
+    headers = {'Authorization': f'Bearer {token}'}
+    payload = {"long_url": user_url}
+    response = requests.post(url, headers=headers, json=payload)
+    response.raise_for_status()
+    short_link = response.json()
+    bitlink = short_link['link']
+    return bitlink
+
+def count_cliks(user_url, token):
+    parsed_url = urlparse(user_url)
+    id_bitly = parsed_url.netloc+parsed_url.path
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{id_bitly}/clicks/summary'
+    headers = {'Authorization': f'Bearer {token}'}
+    params = {
+        'unit': 'day',
+        'units': '-1',
+    }
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    answer = response.json()
+    clicks_count = answer['total_clicks']
+    return clicks_count
+
+def check_bitlink(user_url, token):
+    parsed_url = urlparse(user_url)
+    id_bitly = parsed_url.netloc+parsed_url.path
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{id_bitly}'
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.get(url, headers=headers)
+    return response.ok
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('url', help='выбор URL')
+    return parser
+
+if __name__ == "__main__":
+    load_dotenv()
+    BITLY_TOKEN = os.getenv('TOKEN')
+    parser = create_parser()
+    args = parser.parse_args()
+    user_url = args.url
+    check_link(user_url, BITLY_TOKEN)
